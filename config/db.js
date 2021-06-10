880cb40ba06f8f8162f8e4f728ddb65d96a9be74
Daniel Nguyen <danielnuwin@gmail.com>
2021-06-09 17:31:01 -0700

    Set Up Express, MongoDB

---
+++ config/db.js
@@ -0,0 +1,21 @@
+/**MONGODB CONNECTION */
+
+const mongoose = require('mongoose'); //Require(), config.get()??
+const config = require('config');
+const db = config.get('mongoURI');
+
+//Async Await always wrap in try catch block
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true 
+        }); //since connect returns a promise put await before it
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.log(err.message);
+        process.exit(1); //To fail application and exit it with failure. 
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express'); //Express server up and running
+const connectDB = require('./config/db');
 
 const app = express(); //initialize app variable with express
 
-app.get('/',(req, res) => res.send('API Running')); //take app variable and listen on a port
+//Connect Database
+connectDB();
+
+//Init Middleware to get data in request.body from routes
+app.use(express.json({extended: false}))
+
+app.get('/', (req, res) => res.send('API Running')); //take app variable and listen on a port
+
+//Define Routes   
+app.use('/api/users', require('./routes/api/users')); //Creating endpoints to point to file
+app.use('/api/auth', require('./routes/api/auth')); //Creating endpoints to point to file
+app.use('/api/profile', require('./routes/api/profile')); //Creating endpoints to point to file
+app.use('/api/posts', require('./routes/api/posts')); //Creating endpoints to point to file
 
 const PORT = process.env.PORT || 5000; // //take app variable and listen on a port, if no ENV set then default to port 5000. 
 
